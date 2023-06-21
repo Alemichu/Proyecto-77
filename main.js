d4937b356fbd386edeaf8457163b4aa6a12b2e7b
Alemichu <126122041+Alemichu@users.noreply.github.com>
2023-06-21 14:58:45 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var hola_mundo = "HELLO WORLD";
+var first_name = "ALEJO ";
+var last_name = "MENDOZA";
+var hola = "HELLO ";
+var mundo = "WORLD";
+var funcion = hola.concat(mundo);
+var description = "MY FIRST PROYECT, CONSOLE LOG";
+var num_1 = 12;
+var num_2 = 4;
+var suma = num_1 + num_2;
+
+console.log(suma);
+console.log("HOLA COMO ESTAS", hola_mundo, "MARAVILLOSO");
+console.log("HOLA MUNDO");
+console.log(funcion);
+console.log(first_name.concat(last_name));
+console.log(description);
